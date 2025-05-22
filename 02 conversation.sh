91201658a0e6724d5af1e92bbeb5e3b2615a37cf
Saiteja <saitejadommeti345@gmail.com>
2025-05-23 00:03:02 +0530

    jai hind

---
+++ 02 conversation.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo IND : hi pak how are you?
+echo Pak : why are asking ?
+echo IND : are you happy now after this cheap terror attacks 
+echo Pak : no, i am not doing these attacks terror are not from my country.
+echo IND : oho, okay i will do one opertion called "SINDOOR" on terrors 
+echo Pak : why this operation and what is this "SINDOOR"
+echo IND : it is a Revenge on terros in pak. 
