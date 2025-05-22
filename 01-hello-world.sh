35e17f3bdd492f912014f8918c081ddb9e3bf6e5
Saiteja <saitejadommeti345@gmail.com>
2025-05-22 23:28:00 +0530

    hello world started

---
+++ 01-hello-world.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+IND : hi pak how are you?
+Pak : why are asking ?
+IND : are you happy now after this cheap terror attacks 
+Pak : no, i am doing these attacks terror are not from my country.
+IND : oho, okay i will do one opertion called "SINDOOR" on terrors 
+Pak : why this operation and what is this "SINDOOR"
