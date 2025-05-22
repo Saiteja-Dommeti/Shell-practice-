9f59a98dd2f6784f5bf0a3c9f8f0959b06f21a89
Saiteja <saitejadommeti345@gmail.com>
2025-05-23 01:03:13 +0530

    i love my country

---
+++ 06-data-types.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+Target01=10 
+Target02=20
+
+#OPS= $Target01+$Target02
+
+TIMESTAMP=$(date)
+
+echo "script executed at :$TIMESTAMP"
+
+OPS=$(($Target01+$Target02))
+
+echo "sum of $Target01 and $Target02 " is : $OPS"
