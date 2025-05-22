30c82481e06c8487a064efc79355a311be76d210
Saiteja <saitejadommeti345@gmail.com>
2025-05-23 00:18:03 +0530

    i love my country

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+COUNTRY1=INDIA
+COUNTRY2=PAKISTAN
+echCOUNTRY1o  : hi pak how are you?
+echo COUNTRY2 : why are asking ?
+echo COUNTRY1 : are you happy now after this cheap terror attacks 
+echo COUNTRY2 : no, i am not doing these attacks terror are not from my country.
+echo COUNTRY1 : oho, okay i will do one opertion called "SCOUNTRY1OOR" on terrors 
+echo COUNTRY2 : why this operation and what is this "SCOUNTRY1OOR"
+echo COUNTRY1 : it is a Revenge on terros in pak. 
