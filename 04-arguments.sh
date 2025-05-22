7f85ccfb5b36f4ed44dd810ccd2b1e2aa2050588
Saiteja <saitejadommeti345@gmail.com>
2025-05-23 00:34:27 +0530

    i love my country

--- 04-arguments.sh
+++ 04-arguments.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-COUNTRY1=$INDIA
-COUNTRY2=$PAKISTAN
+COUNTRY1=INDIA
+COUNTRY2=PAKISTAN
 
-echo COUNTRY1  : hi pak how are you?
-echo COUNTRY2 : why are asking ?
-echo COUNTRY1 : are you happy now after this cheap terror attacks 
-echo COUNTRY2 : no, i am not doing these attacks terror are not from my country.
-echo COUNTRY1 : oho, okay i will do one opertion called "SCOUNTRY1OOR" on terrors 
-echo COUNTRY2 : why this operation and what is this "SCOUNTRY1OOR"
-echo COUNTRY1 : it is a Revenge on terros in pak. 
+echo $$COUNTRY1  : hi pak how are you?
+echo $COUNTRY2 : why are asking ?
+echo $COUNTRY1 : are you happy now after this cheap terror attacks 
+echo $COUNTRY2 : no, i am not doing these attacks terror are not from my country.
+echo $COUNTRY1 : oho, okay i will do one opertion called "S$COUNTRY1OOR" on terrors 
+echo $COUNTRY2 : why this operation and what is this "S$COUNTRY1OOR"
+echo $COUNTRY1 : it is a Revenge on terros in pak. 
