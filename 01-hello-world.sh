335ce146efb2aad29f6002313a3d2a1e46a2bf9f
Saiteja <saitejadommeti345@gmail.com>
2025-05-22 23:55:48 +0530

    jai hind

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-IND : hi pak how are you?
-Pak : why are asking ?
-IND : are you happy now after this cheap terror attacks 
-Pak : no, i am doing these attacks terror are not from my country.
-IND : oho, okay i will do one opertion called "SINDOOR" on terrors 
-Pak : why this operation and what is this "SINDOOR"
-IND : it is a Revenge on terros in pak. 
+echo IND : hi pak how are you?
+echo Pak : why are asking ?
+echo IND : are you happy now after this cheap terror attacks 
+echo Pak : no, i am doing these attacks terror are not from my country.
+echo IND : oho, okay i will do one opertion called "SINDOOR" on terrors 
+echo Pak : why this operation and what is this "SINDOOR"
+echo IND : it is a Revenge on terros in pak. 
