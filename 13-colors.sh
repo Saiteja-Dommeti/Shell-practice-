ebef6201a3be7f01958d14a17517a4846faa3e23
Saiteja <saitejadommeti345@gmail.com>
2025-05-25 16:04:45 +0530

    HELLO COLORS

--- 13-colors.sh
+++ 13-colors.sh
@@ -3,6 +3,7 @@
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
+M="\e[38m"
 Y="\e[33m"
 N="\e[0m"
 
@@ -11,5 +12,5 @@ then
     echo -e "$R ERROR:: please run this script with root access $N"
     exit 1 #give other than 0 to 127
 else
-     echo -e "$G your are running with root access $N"
+     echo -e "$M your are running with root access $N"
 fi       
